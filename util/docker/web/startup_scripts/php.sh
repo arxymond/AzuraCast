4d7a237f964cf2643a1ec5398239b0f3b868f357
Buster "Silver Eagle" Neece <buster@busterneece.com>
2021-03-26 20:34:42 -0500

    Update build commands for PHP 8.0.

--- util/docker/web/startup_scripts/php.sh
+++ util/docker/web/startup_scripts/php.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Copy the php.ini template to its destination.
-dockerize -template "/etc/php/7.4/fpm/05-azuracast.ini.tmpl:/etc/php/7.4/fpm/conf.d/05-azuracast.ini" -template "/etc/php/7.4/fpm/www.conf.tmpl:/etc/php/7.4/fpm/pool.d/www.conf" cp /etc/php/7.4/fpm/conf.d/05-azuracast.ini /etc/php/7.4/cli/conf.d/05-azuracast.ini
+dockerize -template "/etc/php/8.0/fpm/05-azuracast.ini.tmpl:/etc/php/8.0/fpm/conf.d/05-azuracast.ini" \
+  -template "/etc/php/8.0/fpm/www.conf.tmpl:/etc/php/8.0/fpm/pool.d/www.conf"
+
+cp /etc/php/8.0/fpm/conf.d/05-azuracast.ini /etc/php/8.0/cli/conf.d/05-azuracast.ini
